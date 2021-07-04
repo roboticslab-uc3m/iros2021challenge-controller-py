1347d4887677cb222bee1913b52f8a2be5a26cc1
jgvictores <jcgvicto@gmail.com>
2021-07-04 20:09:39 +0200

    add robot step loop

--- participant_controller.py
+++ participant_controller.py
@@ -1,5 +1,10 @@
 from controller import Robot
 
+# Initialize the robot
 robot = Robot()
+timestep = int(robot.getBasicTimeStep())
 
-print("Hello World from Python!")
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    print("Hello World from Python!")
