0767b68434ec7de5e390bb9329a0d1e40311f53e
jgvictores <jcgvicto@gmail.com>
2021-07-04 20:40:05 +0200

    add velocities

--- participant_controller.py
+++ participant_controller.py
@@ -8,7 +8,15 @@ timestep = int(robot.getBasicTimeStep())
 motor_left = robot.getDevice('wheel_left_joint')
 motor_right = robot.getDevice('wheel_right_joint')
 
+motor_left.setVelocity(0.0)
+motor_right.setVelocity(0.0)
+
+motor_left.setPosition(float('inf'))
+motor_right.setPosition(float('inf'))
+
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
     print("Hello World from Python!")
+    motor_left.setVelocity(5.0)
+    motor_right.setVelocity(5.0)
