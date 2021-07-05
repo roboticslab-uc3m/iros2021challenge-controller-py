26ffc3d72fa3da494ffbb3b51d14d78150468634
jgvictores <jcgvicto@gmail.com>
2021-07-05 10:41:43 +0200

    add Obtain waypoints

--- participant_controller.py
+++ participant_controller.py
@@ -4,6 +4,14 @@ from controller import Robot
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 
+# Obtain waypoints
+waypoints = []
+waypoints_string = robot.getCustomData().split()
+for i in range(10):
+    waypoints_element = [float(waypoints_string[2*i]), float(waypoints_string[2*i+1])]
+    waypoints.append(waypoints_element)
+print('Waypoints:', waypoints)
+
 # Initialize devices
 motor_left = robot.getDevice('wheel_left_joint')
 motor_right = robot.getDevice('wheel_right_joint')
@@ -20,6 +28,6 @@ gps.enable(timestep)
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    print("Hello World from Python!", gps.getValues())
+    print('Hello World from Python!', gps.getValues())
     motor_left.setVelocity(5.0)
     motor_right.setVelocity(5.0)
