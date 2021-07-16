6abf435614a511a445169b7851b8ea03de202919
jgvictores <jcgvicto@gmail.com>
2021-07-16 10:41:22 +0200

    add imu

--- participant_controller.py
+++ participant_controller.py
@@ -16,6 +16,7 @@ print('Waypoints:', waypoints)
 motor_left = robot.getDevice('wheel_left_joint')
 motor_right = robot.getDevice('wheel_right_joint')
 gps = robot.getDevice('gps')
+imu = robot.getDevice('inertial unit')
 
 motor_left.setVelocity(0.0)
 motor_right.setVelocity(0.0)
@@ -24,11 +25,13 @@ motor_left.setPosition(float('inf'))
 motor_right.setPosition(float('inf'))
 
 gps.enable(timestep)
+imu.enable(timestep)
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    print('Hello World from Python!', gps.getValues())
+    imu_rads = imu.getRollPitchYaw()
+    print('Hello World from Python!', gps.getValues(), [x*180.0/3.14159 for x in imu_rads])
     motor_left.setVelocity(5.0)
     motor_right.setVelocity(5.0)
     if gps.getValues()[0] > -1.5:
